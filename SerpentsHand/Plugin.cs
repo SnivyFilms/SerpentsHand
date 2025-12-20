54bfe05211c50e5fcc3f8628fb04c55b8853ff7f
Snivy Films <120346554+SnivyFilms@users.noreply.github.com>
2025-12-20 17:04:43 -0500

    Something Something SL update

--- SerpentsHand/Plugin.cs
+++ SerpentsHand/Plugin.cs
@@ -13,8 +13,8 @@ namespace SerpentsHand
 	{
 		public override string Name => "Serpents Hand";
 		public override string Author => "yanox, Michal78900, Marco15453, Vicious Vikki & Misfiy";
-		public override Version RequiredExiledVersion => new(9, 11, 0);
-		public override Version Version => new(9, 1, 6);
+		public override Version RequiredExiledVersion => new(9, 12, 0);
+		public override Version Version => new(9, 1, 7);
 
 		public static Plugin Instance;
 
